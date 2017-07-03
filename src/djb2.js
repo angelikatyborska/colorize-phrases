1295ae26b3ea798963150ae7e42c915972b51f16
Angelika Tyborska <atyborska93@gmail.com>
2017-07-03 13:33:04 +0200

    Avoid negative values for consistency

--- src/djb2.js
+++ src/djb2.js
@@ -1,7 +1,9 @@
 function djb2(string) {
-  return string
+  return Math.abs(
+    string
     .split('')
     .reduce(function(acc, char) {
       return ((acc << 5) + acc) + char.charCodeAt(0);
-    }, 5381);
+    }, 5381)
+  );
 }
--- src/djb2Square.js
+++ src/djb2Square.js
@@ -1,7 +1,9 @@
 function djb2Square(string) {
-  return string
+  return Math.abs(
+    string
     .split('')
     .reduce(function(acc, char) {
       return ((acc << 5) + acc) + Math.pow(char.charCodeAt(0), 2);
-    }, 5381);
+    }, 5381)
+  );
 }
